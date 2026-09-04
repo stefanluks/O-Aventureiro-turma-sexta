cd679bd147cc06e0a00075b5df80cf24be171699
Stefan Lucas <stefan.luks.as@gmail.com>
2026-09-04 20:01:58 -0300

    add meia aula 04.09

--- Assets/Scripts/Jogador.cs
+++ Assets/Scripts/Jogador.cs
@@ -7,16 +7,21 @@ public class Jogador : MonoBehaviour
     public int velocidade;
     private Vector2 direcao;
     public Rigidbody2D rig;
+    public Animator animador;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Hello, World!");
+        rig = GetComponent<Rigidbody2D>();
+        animador = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         rig.linearVelocity = direcao * velocidade;
+        animador.SetFloat("eixoX", direcao.x);
+        animador.SetFloat("eixoY", direcao.y);
+        animador.SetBool("correndo", direcao != Vector2.zero);
     }
 
     public void OnMove(InputAction.CallbackContext input)
